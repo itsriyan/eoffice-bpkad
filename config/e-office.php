d6e393949766cc5b4c6a9d90b448e83fd1c128b9
riyan purnama <riyan@sirclo.com>
2025-10-30 15:12:12 +0700

    finis

--- config/e-office.php
+++ config/e-office.php
@@ -4,13 +4,12 @@ return [
     'arsip_token' => env('E_OFFICE_ARSIP_TOKEN', ''),
     // WhatsApp Meta API configuration
     'whatsapp' => [
-        'base_url' => env('E_OFFICE_WA_BASE_URL', 'https://graph.facebook.com/v19.0'),
+        'base_url' => env('E_OFFICE_WA_BASE_URL', 'https://graph.facebook.com/v24.0'),
         'phone_number_id' => env('E_OFFICE_WA_PHONE_NUMBER_ID', ''),
         'access_token' => env('E_OFFICE_WA_ACCESS_TOKEN', ''),
         'default_language' => env('E_OFFICE_WA_DEFAULT_LANG', 'id'),
         'default_template' => env('E_OFFICE_WA_DEFAULT_TEMPLATE', 'surat_masuk_baru'),
         'timeout' => env('E_OFFICE_WA_TIMEOUT', 10),
-        'verify_token' => env('E_OFFICE_WA_VERIFY_TOKEN', 'change-me'),
         'rate_limit' => [
             'switch_per_minute' => env('E_OFFICE_WA_RATE_SWITCH', 12),
             'help_per_minute' => env('E_OFFICE_WA_RATE_HELP', 6),
